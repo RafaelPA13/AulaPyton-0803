6185e3648f0310a723d65571460bdff7d8a3e91d
Rafael Porto Annunciato <84384192+RafaelPA13@users.noreply.github.com>
2024-03-08 20:28:47 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 '''Calculo de nota'''
-'''nota = int(input("Digite a sua nota: "))
+nota = int(input("Digite a sua nota: "))
 if nota >= 6:
     print('Aprovado')
 elif nota < 6 and nota >=4:
     print('Exame')
 else:
-    print('Reprovado')'''
+    print('Reprovado')
 
 '''Cálculo de imposto de renda'''
-'''salario = float(input('Digite o seu salário: '))
+salario = float(input('Digite o seu salário: '))
 if salario <= 1903.99:
     print("Você esta isento de imposto")
 elif salario <= 2826.65:
@@ -18,4 +18,4 @@ elif salario <= 3751.05:
 elif salario <= 4664.68:
     print(f"O desconto foi de R${(salario * 0.225)}, seu salario é R${salario - (salario * 0.225)}")
 else:
-    print(f"O desconto foi de R${(salario * 0.275)}, seu salario é R${salario - (salario * 0.275)}")'''
+    print(f"O desconto foi de R${(salario * 0.275)}, seu salario é R${salario - (salario * 0.275)}")
