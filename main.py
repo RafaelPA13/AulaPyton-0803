80972789cf4ffaef481109842d97b4cd2294b1ea
Rafael Porto Annunciato <84384192+RafaelPA13@users.noreply.github.com>
2024-03-08 21:02:36 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 '''Calculo de nota'''
-'''nota = int(input("Digite a sua nota: "))
+nota = int(input("Digite a sua nota: "))
 if nota >= 6:
     print('Aprovado')
 elif nota < 6 and nota >=4:
     print('Exame')
 else:
-    print('Reprovado')'''
+    print('Reprovado')
 
 '''Cálculo de imposto de renda'''
-'''salario = float(input('Digite o seu salário: '))
+salario = float(input('Digite o seu salário: '))
 if salario <= 1903.99:
     print("Você esta isento de imposto")
 elif salario <= 2826.65:
@@ -18,10 +18,10 @@ elif salario <= 3751.05:
 elif salario <= 4664.68:
     print(f"O desconto foi de R${(salario * 0.225)}, seu salario é R${salario - (salario * 0.225)}")
 else:
-    print(f"O desconto foi de R${(salario * 0.275)}, seu salario é R${salario - (salario * 0.275)}")'''
+    print(f"O desconto foi de R${(salario * 0.275)}, seu salario é R${salario - (salario * 0.275)}")
 
 '''Exercício 1'''
-'''X = int(input('Digite um número X: '))
+X = int(input('Digite um número X: '))
 Y = int(input('Digite um número Y: '))
 
 if X > Y:
@@ -29,30 +29,29 @@ if X > Y:
 elif Y > X:
     print(f'O número maior é {Y}')
 else:
-    print('Os valores que você digitou são iguais')'''
+    print('Os valores que você digitou são iguais')
 
 '''Exercício 2'''
-'''ano = int(input('Digite o ano que você nasceu: '))
+ano = int(input('Digite o ano que você nasceu: '))
 ano_de_voto = 2024 - ano
 
 if ano_de_voto >= 18:
     print('Esse ano você poderá votar.')
 else:
-    print('Esse ano você não pode voltar')'''
+    print('Esse ano você não pode voltar')
 
 '''Exercício 3'''
-'''senha = int(input('Digete a senha:'))
+senha = int(input('Digete a senha:'))
 
 if senha == 1234:
     print('Acesso permitido')
 else:
     print('Acesso negado')
-'''
 
 '''Exercício 4'''
-'''macas = int(input('Quantas maçãs você comprou'))
+macas = int(input('Quantas maçãs você comprou'))
 
 if macas < 12:
     print(f'O valor das maçãs é R${macas * 0.3}')
 else:
-    print(f'O valor das maçãs é R${macas * 0.25}')'''
+    print(f'O valor das maçãs é R${macas * 0.25}')
